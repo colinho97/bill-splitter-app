741845f4edcfd4b58e40137713e720f9d1434a27
Colin Ho <colinho97@gmail.com>
2022-06-21 18:09:31 -0400

    added in the splitter component and fixed issues with the balances calculating and showing on the page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { useEffect, useState } from 'react';
 
 // Importing Components
 import Header from './Header';
+import Splitter from './Splitter';
 
 
 
@@ -24,7 +25,7 @@ function App() {
         nameTwo: "",
         nameThree: "",
         nameFour: "",
-        nameCheckbox: false,
+        nameCheckbox: 'nameOneCheckbox',
         bill: 0,
     })
 
@@ -32,37 +33,42 @@ function App() {
         console.log(diners);
     }, [diners]);
 
-    const handleNameInputChange = (event) => {
-        // grab the contents of the box
-        // set it to the userInput stateful variable
-        setNameInput(event.target.value);
-        console.log(event.target.value);
-    }
+    // const handleNameInputChange = (event) => {
+    //     // grab the contents of the box
+    //     // set it to the userInput stateful variable
+    //     setNameInput(event.target.value);
+    //     console.log(event.target.value);
+    // }
 
-    const handleBillInputChange = (event) => {
-        // grab the contents of the box
-        // set it to the userInput stateful variable
-        setBillInput(event.target.value);
-        console.log(event.target.value);
-    }
+    // const handleBillInputChange = (event) => {
+    //     // grab the contents of the box
+    //     // set it to the userInput stateful variable
+    //     setBillInput(event.target.value);
+    //     console.log(event.target.value);
+    // }
 
     const handleInputChange = (event) => {
-        const target = event.target;
-        const value = target.type === 'radio' ? target.checked : target.value;
-        const name = target.type === 'radio' ? target.id : target.name;
+        
 
-        console.log(event);
-        setDiners({
-            ...diners,
-            [name]: value
-        });
+        if (event.target.type === 'radio') {
+            setDiners({
+                ...diners,
+                nameCheckbox: event.target.id,
+            });
+
+        } else if (event.target.type === 'text' || event.target.type === 'number') {
+            const target = event.target;
+            const value = target.value;
+            const name = target.name;
+
+            console.log(event);
+            setDiners({
+                ...diners,
+                [name]: value
+            });
+        }
     }
 
-    // const handleCheckboxChange = (event) => {
-    //     // console.log(event);
-    //     console.log(event.target.checked);
-    //     // setChecked(event.target.checked);
-    // }
 
 
     // create an event listener that will handle the user clicking 'Next'
@@ -76,8 +82,14 @@ function App() {
         push(dbRef, diners);
         // push(dbRef, billInput);
         // clear the input (reset the state of userInput to an empty string)
-        setNameInput('');
-        setBillInput(null);
+        setDiners({
+            nameOne: "",
+            nameTwo: "",
+            nameThree: "",
+            nameFour: "",
+            nameCheckbox: 'nameOneCheckbox',
+            bill: 0,
+        })
     }
 
 
@@ -221,6 +233,7 @@ function App() {
                             <button className="nextButton" type="submit" onClick={handleSubmit}>Next</button>
                         </div>
                     </form>
+                    <Splitter />
                 </div>
             </div>
         </>
@@ -240,7 +253,7 @@ export default App;
     //Name Inputs
         //Store input as an object in an array, then store it in Firebase
         //Include a button next to name input field, which onclick, gives user option to add another name field
-        //Make form fields added 'required'
+        //Make form fields adstartded 'required'
         //Add 'Next' button which is the submission of the form
             //On submission of the form, change the useState of the form to take name values entered and push them to the array in firebase using push()
 
---
+++ src/Splitter.js
@@ -0,0 +1,96 @@
+// Importing Firebase 
+import firebase from './firebase';
+import { getDatabase, ref, onValue, push, remove } from 'firebase/database'
+
+import { useEffect, useState } from 'react';
+
+
+const Splitter = (props) => {
+
+    const [balances, setBalances] = useState([]);
+
+    useEffect(() => {
+        // create a variable that holds our database details (getDatabase)
+        const database = getDatabase(firebase);
+
+        // create a variable that makes a reference to our database (ref)
+        const dbRef = ref(database);
+
+        // we need add our onValue event listener so that it will run some code whenever our database value changes
+        onValue(dbRef, (response) => {
+            // get our database values
+            // .val() is a Firebase module that gets us the information we want from the reponse
+            const data = response.val();
+            console.log(data);
+            // our data is an object, so we need to iterate through it using a for in loop to access each book name and turn it into an array
+
+            const newState = [];
+            for (let key in data) {
+                // inside our loop we push each book name to our newState array which we have already created
+                newState.push(
+                    {
+                        key: key,
+                        name: data[key]
+                        // key:props.diners,
+                    }
+                )
+            }
+
+            setBalances(newState);
+            console.log(balances);
+
+        });
+    }, []);
+    // props.handleSubmit
+
+    return (
+        <>
+            <h2>Balances</h2>
+            <ul>
+                {
+                balances.map((balance) => {
+                    return (
+                        <li key={balance.key}>
+                            <p>Split ID: {balance.key}</p>
+                            <p>
+                                <span className="splitName">{balance.name.nameOne}: </span>
+                                {
+                                    balance.name.nameCheckbox === 'nameOneCheckbox'
+                                    ? <span className="nonSplitAmount">+ ${balance.name.bill/4*3}</span>
+                                    : <span className="splitAmount">- ${balance.name.bill/4}</span>
+                                }
+                            </p>
+                            <p>
+                                <span className="splitName">{balance.name.nameTwo}: </span>
+                                {
+                                    balance.name.nameCheckbox === 'nameTwoCheckbox'
+                                    ? <span className="nonSplitAmount">+ ${balance.name.bill/4*3}</span>
+                                    : <span className="splitAmount">- ${balance.name.bill/4}</span>
+                                }
+                            </p>
+                            <p>
+                                <span className="splitName">{balance.name.nameThree}: </span>
+                                {
+                                    balance.name.nameCheckbox === 'nameThreeCheckbox'
+                                    ? <span className="nonSplitAmount">+ ${balance.name.bill/4*3}</span>
+                                    : <span className="splitAmount">- ${balance.name.bill/4}</span>
+                                }
+                            </p>
+                            <p>
+                                <span className="splitName">{balance.name.nameFour}: </span>
+                                {
+                                    balance.name.nameCheckbox === 'nameFourCheckbox'
+                                    ? <span className="nonSplitAmount">+ ${balance.name.bill/4*3}</span>
+                                    : <span className="splitAmount">- ${balance.name.bill/4}</span>
+                                }
+                            </p>
+                        </li>
+                    )
+                })
+                }
+            </ul>
+        </>
+    )
+}
+
+export default Splitter;
