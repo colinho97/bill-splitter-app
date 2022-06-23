362d2958e1e5e2fc0aae59a2baa09e1a0211bfa7
Colin Ho <colinho97@gmail.com>
2022-06-23 01:18:31 -0400

    making final adjustments to main branch before submission

--- src/App.js
+++ src/App.js
@@ -17,9 +17,6 @@ import Splitter from './Splitter';
 function App() {
 
     // initialize our stateful variables
-    const [nameInput, setNameInput] = useState([]);
-    const [billInput, setBillInput] = useState(null);
-    const [checked, setChecked] = useState(false);
     const [diners, setDiners] = useState({
         nameOne: "",
         nameTwo: "",
@@ -29,23 +26,6 @@ function App() {
         bill: 0,
     })
 
-    useEffect(() => {
-        console.log(diners);
-    }, [diners]);
-
-    // const handleNameInputChange = (event) => {
-    //     // grab the contents of the box
-    //     // set it to the userInput stateful variable
-    //     setNameInput(event.target.value);
-    //     console.log(event.target.value);
-    // }
-
-    // const handleBillInputChange = (event) => {
-    //     // grab the contents of the box
-    //     // set it to the userInput stateful variable
-    //     setBillInput(event.target.value);
-    //     console.log(event.target.value);
-    // }
 
     const handleInputChange = (event) => {
         
@@ -61,7 +41,6 @@ function App() {
             const value = target.value;
             const name = target.name;
 
-            console.log(event);
             setDiners({
                 ...diners,
                 [name]: value
@@ -73,15 +52,13 @@ function App() {
 
     // create an event listener that will handle the user clicking 'Next'
     const handleSubmit = (event) => {
-        // prevent default (page from refreshing)
         event.preventDefault();
-        // create a reference to the database
+
         const database = getDatabase(firebase);
         const dbRef = ref(database);
-        // push the value of userInput state to the database
+
         push(dbRef, diners);
-        // push(dbRef, billInput);
-        // clear the input (reset the state of userInput to an empty string)
+
         setDiners({
             nameOne: "",
             nameTwo: "",
@@ -163,12 +140,9 @@ function App() {
                                         <input
                                             className='checkboxInput'
                                             type="radio"
-                                            //Change name below to match one in input to pass checked value to firebase
                                             name='nameCheckbox'
                                             value={diners.nameCheckbox}
                                             id='nameOneCheckbox'
-                                            // Need to change this checked prop as I cant use same state for name one and two
-
                                             onChange={handleInputChange}
                                         />
                                         <span className="checkmark"></span>
@@ -179,7 +153,6 @@ function App() {
                                         <input
                                             className='checkboxInput'
                                             type="radio"
-                                            //Change name below to match one in input to pass checked value to firebase
                                             name='nameCheckbox'
                                             value={diners.nameCheckbox}
                                             id='nameTwoCheckbox'
@@ -193,7 +166,6 @@ function App() {
                                         <input
                                             className='checkboxInput'
                                             type="radio"
-                                            //Change name below to match one in input to pass checked value to firebase
                                             name='nameCheckbox'
                                             value={diners.nameCheckbox}
                                             id='nameThreeCheckbox'
@@ -207,7 +179,6 @@ function App() {
                                         <input
                                             className='checkboxInput'
                                             type="radio"
-                                            //Change name below to match one in input to pass checked value to firebase
                                             name='nameCheckbox'
                                             value={diners.nameCheckbox}
                                             id='nameFourCheckbox'
@@ -242,29 +213,3 @@ function App() {
 
 export default App;
 
-
-
-
-
-//Pseudo Code
-//--------------------------
-
-//Landing Page with form element to take user inputs
-    //Name Inputs
-        //Store input as an object in an array, then store it in Firebase
-        //Include a button next to name input field, which onclick, gives user option to add another name field
-        //Make form fields adstartded 'required'
-        //Add 'Next' button which is the submission of the form
-            //On submission of the form, change the useState of the form to take name values entered and push them to the array in firebase using push()
-
-    //How much was the bill?
-        //Input for bill amount which takes an input with parseInt.
-        //useState to track the state of the input received from user
-        //useEffect which uses the state change of the input state, which will perform a calculation using the bill amount input divided by the length of the array pushed to firebase in the first page
-        //Perform function in Balances component which allocates the calculation above to each name in the Firebase array as a key: balance with a value of negative(calculated amount)
-        //Take checkbox part in  form and use that to allocate a Paid: true/false key value pair to the corresponding name, which will add the total bill amount to that name
-
-    //Display Balances
-        //map through the name array in firebase and display object names which represents each person
-        //map through key value pair of balance: $amount
-        //using the map, create divs which hold both values and shows on the page on render
--- src/Splitter.js
+++ src/Splitter.js
@@ -10,47 +10,36 @@ const Splitter = (props) => {
     const [balances, setBalances] = useState([]);
 
     useEffect(() => {
-        // create a variable that holds our database details (getDatabase)
         const database = getDatabase(firebase);
-
-        // create a variable that makes a reference to our database (ref)
         const dbRef = ref(database);
 
-        // we need add our onValue event listener so that it will run some code whenever our database value changes
         onValue(dbRef, (response) => {
-            // get our database values
-            // .val() is a Firebase module that gets us the information we want from the reponse
+
             const data = response.val();
-            console.log(data);
-            // our data is an object, so we need to iterate through it using a for in loop to access each book name and turn it into an array
 
             const newState = [];
             for (let key in data) {
-                // inside our loop we push each book name to our newState array which we have already created
                 newState.push(
                     {
                         key: key,
                         name: data[key]
-                        // key:props.diners,
                     }
                 )
             }
 
             setBalances(newState);
-            console.log(balances);
 
         });
     }, []);
-    // props.handleSubmit
 
     return (
         <>
             <h2 id="balances">Balances</h2>
-            <ul>
+            <ul className='listOfBalances'>
                 {
                 balances.map((balance) => {
                     return (
-                        <li key={balance.key}>
+                        <li key={balance.key} className="balanceItem">
                             <p>Split ID: {balance.key}</p>
                             <p>
                                 <span className="splitName">{balance.name.nameOne}: </span>
